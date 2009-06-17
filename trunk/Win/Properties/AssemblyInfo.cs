263026f3045e2fbda0c2a9c8cba98e52b638cab3
lmayne <lmayne@b1e27596-5b7e-11de-8d7d-01d876f1962d>
2009-06-17 20:46:29 +0000

    Added code at existing state

---
+++ trunk/Win/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Static Data Script Generator")]
+[assembly: AssemblyDescription("Generates scripts for use with Visual Studio Team Edition for Database Professionals for static data tables")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Static Data Script Generator")]
+[assembly: AssemblyCopyright("Copyright © 2006 & 2007 Leon Mayne")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7af28596-47a7-43df-a2ce-22a9d92cdd37")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ trunk/Win/frmGenerate.Designer.cs
@@ -0,0 +1,156 @@
+namespace StaticGenerator
+{
+    partial class frmGenerate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGenerate));
+            this.clbTables = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnGenerateScripts = new System.Windows.Forms.Button();
+            this.fbdDropFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.chkCreateIndex = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // clbTables
+            // 
+            this.clbTables.CheckOnClick = true;
+            this.clbTables.FormattingEnabled = true;
+            this.clbTables.Location = new System.Drawing.Point(12, 30);
+            this.clbTables.Name = "clbTables";
+            this.clbTables.Size = new System.Drawing.Size(361, 139);
+            this.clbTables.TabIndex = 0;
+            this.clbTables.ThreeDCheckBoxes = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select tables to script:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select drop folder:";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(15, 201);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(326, 20);
+            this.txtFolder.TabIndex = 3;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(347, 202);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(25, 19);
+            this.btnSelectFolder.TabIndex = 4;
+            this.btnSelectFolder.Text = "...";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnGenerateScripts
+            // 
+            this.btnGenerateScripts.Location = new System.Drawing.Point(15, 268);
+            this.btnGenerateScripts.Name = "btnGenerateScripts";
+            this.btnGenerateScripts.Size = new System.Drawing.Size(121, 30);
+            this.btnGenerateScripts.TabIndex = 5;
+            this.btnGenerateScripts.Text = "Generate Scripts";
+            this.btnGenerateScripts.UseVisualStyleBackColor = true;
+            this.btnGenerateScripts.Click += new System.EventHandler(this.btnGenerateScripts_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(303, 273);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(69, 24);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // chkCreateIndex
+            // 
+            this.chkCreateIndex.AutoSize = true;
+            this.chkCreateIndex.Location = new System.Drawing.Point(15, 239);
+            this.chkCreateIndex.Name = "chkCreateIndex";
+            this.chkCreateIndex.Size = new System.Drawing.Size(265, 17);
+            this.chkCreateIndex.TabIndex = 7;
+            this.chkCreateIndex.Text = "Create index script (to paste into post deploy script)";
+            this.chkCreateIndex.UseVisualStyleBackColor = true;
+            // 
+            // frmGenerate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 313);
+            this.Controls.Add(this.chkCreateIndex);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnGenerateScripts);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clbTables);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmGenerate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generate Static Data Management Scripts";
+            this.Load += new System.EventHandler(this.frmGenerate_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clbTables;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnGenerateScripts;
+        private System.Windows.Forms.FolderBrowserDialog fbdDropFolder;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.CheckBox chkCreateIndex;
+    }
+}
