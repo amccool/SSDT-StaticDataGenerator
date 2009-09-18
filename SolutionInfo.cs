74ed8292b74f462c3eb0b88d979b7836ac74f187
lmayne <lmayne@b1e27596-5b7e-11de-8d7d-01d876f1962d>
2009-09-18 10:24:20 +0000

    Updated version number for new release

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.4.1.0")]
